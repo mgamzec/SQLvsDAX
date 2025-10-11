3a9a0f76a5b2a001aa082ac96b6742e85ff0907c
Merve Gamze Çınar <62151645+mgamzec@users.noreply.github.com>
2025-10-11 20:33:57 +0300

    Update q25.sql

--- Q25/q25.sql
+++ Q25/q25.sql
@@ -6,17 +6,43 @@ SELECT
     mt.MovieType,
     COUNT(*) AS CountRows,
     COUNT(mt.MovieType) AS Count_MovieType,
-    COUNT(*) - COUNT(mt.MovieType) AS CountBlank,
+    COUNT(*) - COUNT(mt.MovieType) AS CountBlank, -- Calculates CountBlank by subtracting non-nulls from total rows
     COUNT(distinct mt.MovieType) AS DISTINCTCOUNTNOBLANK,
-    COUNT(distinct ISNULL(mt.MovieType, 999)) AS DISTINCTCOUNT
+    COUNT(distinct ISNULL(mt.MovieType, 999)) AS DISTINCTCOUNT -- Uses a proxy value (999) to count NULLs as a distinct item
 FROM #Movie_table mt
 GROUP BY mt.MovieType
 
-----------------------------------------------------------------
+-- *********************************************************************************
+-- SQL UNPIVOT Operation (for comparison/demonstration purposes)
+-- *********************************************************************************
+SELECT *
+FROM (
+    SELECT
+        count(*) as CountRows,
+        count(mt.MovieType) as Count_MovieType,
+        count(*) - count(mt.MovieType) as CountBlank,
+        count(distinct mt.MovieType) as DISTINCTCOUNTNOBLANK,
+        count(distinct ISNULL(mt.MovieType, 999)) as DISTINCTCOUNT
+    from #Movie_table mt
+) as tbl
+UNPIVOT (
+    Quantity for Fields in (CountRows, Count_MovieType, CountBlank, DISTINCTCOUNTNOBLANK, DISTINCTCOUNT)
+) as unpvt_table;
+
+------------------------------------------------------------------------------------------
 
 EVALUATE
 SUMMARIZECOLUMNS(
     Movie_Table[MovieType],
+    "CountRows", COUNTROWS(Movie_Table),
+    "Count_MovieType", COUNT(Movie_Table[MovieType]),
+    "CountBlank", COUNTBLANK(Movie_Table[MovieType]),
+    "DISTINCTCOUNTNOBLANK", DISTINCTCOUNTNOBLANK(Movie_Table[MovieType]),
+    "DISTINCTCOUNT", DISTINCTCOUNT(Movie_Table[MovieType])
+)
+
+EVALUATE
+ROW(
     "CountRows", COUNTROWS(Movie_Table),
     "Count_MovieType", COUNT(Movie_Table[MovieType]),
     "CountBlank", COUNTBLANK(Movie_Table[MovieType]),
